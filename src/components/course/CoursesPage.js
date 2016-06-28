09eb03716f811099903218a4420d7e9963f34823
Michał Ordon <michal@tehidea.com>
2016-06-28 20:13:16 +0100

    Add a simple form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,10 +3,40 @@ import { Link } from 'react-router';
 
 class CoursesPage extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: '' }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+        <input
+          type="submit"
+          onClick={this.onClickSave}
+          value="Save" />
       </div>
     );
   }

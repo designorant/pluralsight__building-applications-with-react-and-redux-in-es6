1d430f397330f763ca1f1529d043afe340dd39bd
Michał Ordon <michal@tehidea.com>
2016-07-01 00:34:32 +0100

    Instantiate store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />, document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>, 
+  document.getElementById('app')
 );

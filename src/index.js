8390fc3b29a3082227a45d4b65da19641627fec7
Michał Ordon <michal@tehidea.com>
2016-07-08 13:11:48 +0100

    Dispatch load courses action on page load

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
-  </Provider>, 
+  </Provider>,
   document.getElementById('app')
 );

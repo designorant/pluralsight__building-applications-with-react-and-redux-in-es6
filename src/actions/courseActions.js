5532f255068cf3d38af298bca04ce225655b593b
Michał Ordon <michal@tehidea.com>
2016-07-08 12:57:21 +0100

    Create loadCourses thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,16 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }

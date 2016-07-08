384e71eea771a76b79129d07f0b241c6186fdcf3
Michał Ordon <michal@tehidea.com>
2016-07-08 13:00:25 +0100

    Load courses in reducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,8 +2,8 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [...state, Object.assign({}, action.course)];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }

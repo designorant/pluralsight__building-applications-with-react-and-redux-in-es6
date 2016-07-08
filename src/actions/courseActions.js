f400f3afcd42f41d00c916447be39e536f50419c
Michał Ordon <michal@tehidea.com>
2016-07-08 12:14:28 +0100

    Introduce action type constants

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,5 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
-  return { type: 'CREATE_COURSE', course };
+  return { type: types.CREATE_COURSE, course };
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       return [...state, Object.assign({}, action.course)];
     default:
       return state;

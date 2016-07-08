fb5482d14ba8550d31a62911ad10e720df89cc40
Michał Ordon <michal@tehidea.com>
2016-07-08 12:44:53 +0100

    Clean up Courses page container

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -7,23 +7,6 @@ class CoursesPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: '' }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -35,15 +18,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-        <input
-          type="submit"
-          onClick={this.onClickSave}
-          value="Save" />
       </div>
     );
   }

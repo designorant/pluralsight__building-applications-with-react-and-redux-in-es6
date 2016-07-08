4e37b12cc726a3781dfe411a65bce68b1452fdce
Michał Ordon <michal@tehidea.com>
2016-07-08 13:28:56 +0100

    Create Course List component

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
 
@@ -14,10 +15,11 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+    const { courses } = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses} />
       </div>
     );
   }

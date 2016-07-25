2928d5ad19f81de8d61ea0d9ee0c99e14bdfaf63
Michał Ordon <michal@tehidea.com>
2016-07-26 00:42:22 +0100

    Create Save Course thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,10 +5,28 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });

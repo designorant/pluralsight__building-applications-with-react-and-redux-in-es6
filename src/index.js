3050f3f66d0e3d37508cd7b9e87dcbf8bacf7195
Michał Ordon <michal@tehidea.com>
2016-07-26 00:02:06 +0100

    Create author reducer

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
--- src/reducers/authorReducer.js
+++ src/reducers/authorReducer.js
@@ -1,10 +1,10 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function courseReducer(state = initialState.courses, action) {
+export default function authorReducer(state = initialState.authors, action) {
   switch(action.type) {
-    case types.LOAD_COURSES_SUCCESS:
-      return action.courses;
+    case types.LOAD_AUTHORS_SUCCESS:
+      return action.authors;
     default:
       return state;
   }

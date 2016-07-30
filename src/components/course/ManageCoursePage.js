2e3c36a2789f1f61295496866239e9ee49dd2ef5
Michał Ordon <michal@tehidea.com>
2016-07-30 23:36:36 +0100

    Redirect via react router context

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -27,6 +27,7 @@ class ManageCoursePage extends React.Component {
   saveCourse(event) {
     event.preventDefault();
     this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
 
   render() {
@@ -45,6 +46,11 @@ ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
+};
+
+// Pull in the React Router context so router is available on this.context.router
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object // Left optional to avoid linting warning when testing. Behaviour isn't impacted.
 };
 
 function mapStateToProps(state, ownProps) {

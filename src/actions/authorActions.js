f2a6892b644a948453868963b5aa20880c21e6f2
Michał Ordon <michal@tehidea.com>
2016-08-01 00:48:20 +0100

    Call begin AJAX in thunks

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,12 +1,14 @@
 import * as types from './actionTypes';
 import authorApi from '../api/mockAuthorApi';
+import { beginAjaxCall } from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
 }
 
 export function loadAuthors() {
-  return function(dispatch) {
+  return dispatch => {
+    dispatch(beginAjaxCall());
     return authorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error => {

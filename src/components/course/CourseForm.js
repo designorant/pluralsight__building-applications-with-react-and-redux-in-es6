40610c7d9ebf023ebd4660b3ebee2d5bf72a2b3c
Michał Ordon <michal@tehidea.com>
2016-08-01 01:27:04 +0100

    Create form submission loading indicator

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import TextInput from '../common/TextInput';
 import SelectInput from '../common/SelectInput';
 
-const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
+const CourseForm = ({course, allAuthors, onSave, onChange, saving, errors}) => {
   return (
     <form>
       <h1>Manage Course</h1>
@@ -34,8 +34,8 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
         error={errors.length} />
       <input
         type="submit"
-        disabled={loading}
-        value={loading ? 'Saving...' : 'Save'}
+        disabled={saving}
+        value={saving ? 'Saving...' : 'Save'}
         className="btn btn-primary"
         onClick={onSave} />
     </form>
@@ -47,7 +47,7 @@ CourseForm.propTypes = {
   allAuthors: PropTypes.array,
   onSave: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
-  loading: PropTypes.bool,
+  saving: PropTypes.bool,
   errors: PropTypes.object
 };
 
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -10,7 +10,8 @@ class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, props.course), // 'this' keyword ommited since passed on constructor
-      errors: {}
+      errors: {},
+      saving: false
     };
 
     this.updateCourseState = this.updateCourseState.bind(this);
@@ -33,11 +34,13 @@ class ManageCoursePage extends React.Component {
 
   saveCourse(event) {
     event.preventDefault();
+    this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect());
   }
 
   redirect() {
+    this.setState({saving: false});
     this.context.router.push('/courses');
   }
 
@@ -48,7 +51,8 @@ class ManageCoursePage extends React.Component {
         onChange={this.updateCourseState}
         onSave={this.saveCourse}
         course={this.state.course}
-        errors={this.state.errors} />
+        errors={this.state.errors}
+        saving={this.state.saving}/>
     );
   }
 }

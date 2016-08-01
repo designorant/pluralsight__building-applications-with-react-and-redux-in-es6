4d6027459c1c1b6f7560c907fad1fe5c325aa810
Michał Ordon <michal@tehidea.com>
2016-08-01 01:06:16 +0100

    Show and hide preloaded based on status

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { PropTypes } from 'react';
 import Header from './common/Header';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="container-fluid">
-        <Header />
+        <Header
+          loading={this.props.loading} />
         {this.props.children}
       </div>
     );
@@ -15,7 +17,14 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  children: React.PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,20 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import LoadingDots from './LoadingDots';
 
-const Header = () => (
+const Header = ({loading}) => (
   <nav>
   <IndexLink to="/" activeClassName="active">Home</IndexLink>
   {" | "}
   <Link to="/courses" activeClassName="active">Courses</Link>
   {" | "}
   <Link to="/about" activeClassName="active">About</Link>
+  {loading && <LoadingDots interval={100} dots={20} />}
   </nav>
 );
+
+Header.propTypes = {
+  loading: React.PropTypes.bool.isRequired
+};
 
 export default Header;

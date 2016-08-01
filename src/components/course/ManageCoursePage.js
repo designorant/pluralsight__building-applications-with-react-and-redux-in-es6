c829eb2df3b5deb06651fae769a19494cd1180cf
Michał Ordon <michal@tehidea.com>
2016-08-01 01:37:07 +0100

    Finish error handling

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -37,7 +37,11 @@ class ManageCoursePage extends React.Component {
     event.preventDefault();
     this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
-      .then(() => this.redirect());
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({saving: false});
+      });
   }
 
   redirect() {

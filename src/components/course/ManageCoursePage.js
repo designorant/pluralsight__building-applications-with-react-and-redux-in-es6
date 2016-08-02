f1088d1bc80b6d407f01c3a84f621dc564ce66cb
Michał Ordon <michal@tehidea.com>
2016-08-03 00:02:29 +0100

    Add validation, TDD style

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -33,8 +33,26 @@ export class ManageCoursePage extends React.Component {
     return this.setState({course: course});
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+
+    this.setState({errors: errors});
+    return formIsValid;
+  }
+
   saveCourse(event) {
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
     this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())

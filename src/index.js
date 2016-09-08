19e777464bc81cc18987ade1b2f67822dd9f429d
Michał Ordon <michal@tehidea.com>
2016-09-08 23:21:41 +0100

    Setup production ready store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import configureStore from './store/configureStore';
+import configureStore from './store/configureStore'; // eslint-disable-line import/default
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
-  );
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
 }
